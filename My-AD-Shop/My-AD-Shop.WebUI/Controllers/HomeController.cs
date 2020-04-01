aca315201afbfe9a241299fe3e07edcb0056bf64
andyd68 <andy_dingley@btinternet.com>
2020-04-01 17:22:38 +0100

    Added Product Filter

--- My-AD-Shop/My-AD-Shop.WebUI/Controllers/HomeController.cs
+++ My-AD-Shop/My-AD-Shop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using My_AD_Shop.Core.Contracts;
 using My_AD_Shop.Core.Models;
+using My_AD_Shop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,23 @@ namespace My_AD_Shop.WebUI.Controllers
             productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
         }
 
         public ActionResult Details(string Id) {
